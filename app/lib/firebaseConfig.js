4aac86a30b44d761bdcaf5b72b613f1b1077a697
BassamMustafa16 <basam_wadi@yahoo.com>
2025-05-25 18:30:23 +0300

    Google Api Security Issue Solved

--- app/lib/firebaseConfig.js
+++ app/lib/firebaseConfig.js
@@ -7,13 +7,13 @@ import { initializeApp } from "firebase/app";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyA8Dq5TexCLuRBokMbIRjmcqua0UrHxa9g",
-  authDomain: "wallet-app-a0acb.firebaseapp.com",
-  projectId: "wallet-app-a0acb",
-  storageBucket: "wallet-app-a0acb.firebasestorage.app",
-  messagingSenderId: "562563398317",
-  appId: "1:562563398317:web:97209ed829dee7cca86424",
-  measurementId: "G-XQWEDN78YW"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
